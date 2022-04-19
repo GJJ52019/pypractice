d0abbd8ab1913cd6999f9ba4c46146395fe6eb99
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-04-19 11:24:32 -0400

    inital push

---
+++ smallprojects/bagel.py
@@ -0,0 +1,12 @@
+#bagels 
+#by al sweigart
+#markdown followed by gary jenks 3/29/2022
+
+import random 
+
+NUM_DIGITS = 3 # (!) changes the number of digits to be guessed
+MAX_GUESSES = 10 #(!) allows the amount of listed guesses
+
+
+def main():
+    print('''Bagels, a deductive logic game.''' )
