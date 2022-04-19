186e60d401e4ba15aae64ae50142ae28af548365
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-04-19 12:43:21 -0400

    worked on list practice making bicycles list and calling through a message

---
+++ practice/bicycles.py
@@ -0,0 +1,16 @@
+#list and printing them
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+#prints bicycles list then the first element
+print(bicycles)
+print(bicycles[0])
+
+#capitlizes the first letter
+print(bicycles[0].title())
+
+#prints the last element of a list
+print(bicycles[-1])
+
+message = "My first bicyle was a " + bicycles[-1].title() + "."
+
+print(message)
