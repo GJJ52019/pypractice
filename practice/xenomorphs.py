16e050b4686438cffb9c700df9f31eb251b47563
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-04-21 15:49:59 -0400

    list of xenos

---
+++ practice/xenomorphs.py
@@ -0,0 +1,13 @@
+#initialize list 
+xenos = ['pretorian', 'chestburster', 'drone']
+print(xenos)
+
+#change elemnests by calling their location and then setting them with = sign
+xenos[0] = 'facehugger'
+print(xenos)
+
+#append with .append
+xenos.append('pretorian')
+print(xenos)
+
+#insert elements()
