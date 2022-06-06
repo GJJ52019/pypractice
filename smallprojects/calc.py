244192a2b041cc91cf8ab1ed1f203f3d41188db8
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-06-06 12:02:58 -0400

    created calc.py made basic if/else calculator handles user input into two variables to store numbers and one for the operator.

---
+++ smallprojects/calc.py
@@ -0,0 +1,18 @@
+#small basic calculator program 
+
+#forces input into a float if otherwise causes error
+num1 = float(input("Enter first number: "))
+op = input("Enter an operator: ")
+num2 = float(input("Enter second number"))
+
+#if statement to check operator
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Please enter only operators")
