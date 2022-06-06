c3845953c1e803f8d2a9419dd1fb0b0eb44dc290
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-06-06 10:52:22 -0400

    added return file and made example

---
+++ practice/return.py
@@ -0,0 +1,12 @@
+#return practice
+
+#function to return cubed int that is passed in
+# use return to get response from function
+#anything below the return statement does not work
+def cube(num):
+     return num**3
+
+
+results =  cube(2)
+
+print(results)
