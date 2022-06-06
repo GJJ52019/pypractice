2846629a4b5726e8249ee993ff9e14a391c05427
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-06-06 10:46:32 -0400

    basic function example made with hellothere

---
+++ practice/functionpractice.py
@@ -0,0 +1,16 @@
+# basic function practice
+
+#start with def, define function
+# same naming conventions as rest of python
+#need () then :
+#everything inside the function needs to be indented 
+#stuff inside () is a passed variable that is needed to use the function
+def helloThere(insult, name):
+    print("Kenobi!?")
+    print("Hello There " + insult + " " + name)
+
+# must call the funtion to have it run in file
+
+helloThere("stinky","mike")
+
+
