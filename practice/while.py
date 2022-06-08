7637235662333233343742f4b160a540388c5766
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-06-08 16:03:38 -0400

    created while.py for basic practice.

---
+++ practice/while.py
@@ -0,0 +1,10 @@
+# while loop basics and practice
+
+#make a variable to increment 
+#while needs a : then indent to keep what happens in the loop together
+i = 1
+while i <= 10:
+    print(i)
+    i+=1
+
+print("\nloop is finished")
