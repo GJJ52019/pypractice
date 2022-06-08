752889ded9e196fc02ea3b99e0ca39a9d58109e4
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-06-08 16:27:32 -0400

    added additional while loop to keep game going still need to add in guessCount and options to change word.

--- smallprojects/wordGuess.py
+++ smallprojects/wordGuess.py
@@ -5,18 +5,23 @@
 #one user inputs a word for a secret word variable 
 #second user guesses and checks secret word
 
-userSecret = input("Set your secret word: ")
-guess = ""
-turn = 3
-isPlay = True
+userSecret = input("Set your secret word: ") # user inputed secret
+guess = "" #second users guess
 
-while guess != userSecret or turn == 0:
-    guess = input("You have " + str(turn) + " turns left Enter Guess: ")
-    turn -= 1
-    if turn == 0 :
-        break
+isPlay = "y" 
+
+
+while isPlay == "y":
+    turn = 3 # turn counter
+    guessCount = 0
+    while guess != userSecret or turn == 0:
+        guess = input("You have " + str(turn) + " turns left enter an guess: ")
+        turn -= 1
+        if turn <= 0:
+            break
    
-if turn == 0:
-    print("Out of guesses please play again")
-else:
-    print("Nice guess!!")
+    if turn == 0 and guess != userSecret:
+        print("\nOut of guesses would you like to play again\n")
+        isPlay = input('Press "Y" for yes or "N" for no: ').lower()
+    else:
+        print("Nice guess!! You win!!")
