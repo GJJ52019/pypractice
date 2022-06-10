b60597b3c0ada585f928cead8eeed8dd00dd09e3
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-06-10 14:59:29 -0400

    added specific errors to the except error checking

--- practice/errorChecking.py
+++ practice/errorChecking.py
@@ -6,8 +6,11 @@
 # print(num)
 
 #use of try/except to handle invalid inputs
+#best practice is to check for specific errors and print them.
 try:
     num = int(input("Please enter a number: "))
     print(num)
-except:
+except ZeroDivisionError as err: #specified exception catch, as allows the error to be printed as err
+    print(err)
+except ValueError: #input value error 
     print("please only use integers")
