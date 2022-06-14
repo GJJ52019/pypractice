2281645c989d12162effeee30df182f6b40719d5
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2022-06-14 10:07:06 -0400

    added accessing a dictionary within a list to dictionary.py

--- practice/dictionary.py
+++ practice/dictionary.py
@@ -2,7 +2,7 @@
 
 #convert three digit month names to full names ex: jan to january
 
-#dictionaries use {} tuples use () arrays use []
+#dictionaries use {}, tuples use (), arrays use [].
 #Each entry has key: followed by its value (Key: value) for key vauled pairs
 
 # can also use numbers instead of strings for keys.
@@ -30,3 +30,23 @@ print(monthsConversions.get("Dec"))
 
 #when not able to access something use default
 print(monthsConversions.get("fun", "not a default value"))
+
+#dictonaries can also be containted within list
+
+mediaList = [
+    {
+    'movies': ['alien','aliens','alien3'],
+    'games': ['AVP, 2000','AVP,2002','Isolation'],
+    'tvShow' : None,
+    'gamesAndMovies': True
+    }, 
+    { 
+    'movies': ['Toy Story','Touy Story 2','Toy Story 3','Toy Story 4'],
+    'games': ['Toy Story 2'],
+    'tvShow' : ['Buzz lightYear'],
+    'gamesAndMovies': True
+}
+]
+
+#prints the third item in the movies key in the second list option
+print(mediaList[1]['movies'][2])
